1728895452744c0e0b05b30944a3b8a3ff59723a
danieloeb1234 <daniel.loeb@cri-paris.org>
2015-11-08 11:21:44 +0100

    ok

--- exercises/015/solution.py
+++ exercises/015/solution.py
@@ -1,5 +1,3 @@
 import operator
 print(operator.floordiv(245850922, 78256779))
-a = operator.pow(5, 0.5)
-b = operator.add(1, a)
-print(operator.floordiv(b, 2))
+print(operator.floordiv(operator.add(operator.pow(5, 0.5), 1), 2))
