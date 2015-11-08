da59624dce1a7a24015e9ad65c37184494b7e12e
danieloeb1234 <daniel.loeb@cri-paris.org>
2015-11-08 11:25:18 +0100

    ok

--- exercises/015/solution.py
+++ exercises/015/solution.py
@@ -1,3 +1,3 @@
 import operator
-print(operator.floordiv(245850922, 78256779))
-print(operator.floordiv(operator.add(operator.pow(5, 0.5), 1), 2))
+print(operator.truediv(245850922, 78256779))
+print(operator.truediv(operator.add(operator.pow(5, 0.5), 1), 2))
