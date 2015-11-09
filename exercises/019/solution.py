ced673e774fe4b32d6b30327ea0e143b9a4e2294
danieloeb1234 <daniel.loeb@cri-paris.org>
2015-11-09 16:24:24 +0100

    first commit

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -3,4 +3,4 @@ import sys
 if len(sys.argv) <= 2:
     print("usage: python3 solution.py OP1 OP2")
 else:
-    print(operator.add(sys.argv[1], sys.argv[2]))
+    print(operator.add(int(sys.argv[1]), int(sys.argv[2])))
