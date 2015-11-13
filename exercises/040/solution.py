3bad6b24eb1231bc87d9fd7994658c910d3b927b
danieloeb1234 <daniel.loeb@cri-paris.org>
2015-11-13 10:49:44 +0100

    yeah

--- exercises/040/solution.py
+++ exercises/040/solution.py
@@ -1,13 +1,12 @@
-import operator
 T = [int(0), int(2), int(4), int(6), int(8) , int(10),
- int(12), int(14), int(16), int(18), int(20),
- int(22), int(24), int(26), int(28), int(30),
- int(32), int(34), int(36), int(38), int(40),
- int(42), int(44), int(46), int(48), int(50),
- int(52), int(54), int(56), int(58), int(60),
- int(62), int(64), int(66), int(68), int(70),
- int(72), int(74), int(76), int(78), int(80),
- int(82), int(84), int(86), int(88), int(90),
- int(92), int(94), int(96), int(98), int(100)]
+int(12), int(14), int(16), int(18), int(20),
+int(22), int(24), int(26), int(28), int(30),
+int(32), int(34), int(36), int(38), int(40),
+int(42), int(44), int(46), int(48), int(50),
+int(52), int(54), int(56), int(58), int(60),
+int(62), int(64), int(66), int(68), int(70),
+int(72), int(74), int(76), int(78), int(80),
+int(82), int(84), int(86), int(88), int(90),
+int(92), int(94), int(96), int(98), int(100)]
 a = sum(T)
 print(a)
