8552b834a99764fceaa080607144d45a161b7564
danieloeb1234 <daniel.loeb@cri-paris.org>
2015-11-13 17:15:27 +0100

    first com

--- exercises/060/solution.py
+++ exercises/060/solution.py
@@ -1,6 +1,6 @@
 import operator
 A = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
- "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+     "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 for i in range(26):
-        for j in range(26):
-            print(operator.add(A[i], A[j]))
+    for j in range(26):
+        print(operator.add(A[i], A[j]))
