c91d10f587d5c22f484cc51a660679e8e813b710
danieloeb1234 <daniel.loeb@cri-paris.org>
2015-11-20 15:47:56 +0100

    yes!

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -3,5 +3,5 @@ A = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
      "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 for i in range(26):
     for j in range(26):
-        if i > j:
+        if j > i:
             print(operator.add(A[i], A[j]))
