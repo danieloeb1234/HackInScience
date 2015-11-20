a6b06add5a5efe720f5773433740376f7e86fbbb
danieloeb1234 <daniel.loeb@cri-paris.org>
2015-11-20 15:41:09 +0100

    yes!

---
+++ exercises/080/solution.py
@@ -0,0 +1,7 @@
+import operator
+A = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+     "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+for i in range(26):
+    for j in range(26):
+        if i > j:
+            print(operator.add(A[i], A[j]))
