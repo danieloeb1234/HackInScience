dc4455bfed8f72ef1fe9795fd9c8629a71568e81
danieloeb1234 <daniel.loeb@cri-paris.org>
2015-12-04 16:48:08 +0100

    first commit

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -1,7 +1,7 @@
 def love_meet(bob, alice):
-    print(list(set(bob).intersection(alice)))
+    return(list(set(bob).intersection(alice)))
 
 
 def affair_meet(bob, alice, silvester):
     a = list(set(alice).intersection(silvester))
-    print(list(set(a) - set(bob)))
+    return(list(set(a) - set(bob)))
